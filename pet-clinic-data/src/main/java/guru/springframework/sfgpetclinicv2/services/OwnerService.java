dbb53340c1e9a6c8edd349e61328ec233ee7e92d
AndrewH2O <AndrewH2O@users.noreply.github.com>
2020-01-15 22:04:06 +0000

    Created Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicv2/services/OwnerService.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinicv2.services;
+
+import guru.springframework.sfgpetclinicv2.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
