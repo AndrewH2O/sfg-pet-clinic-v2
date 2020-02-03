7072c39e35fd5e2fd86cc6e42171decfb7b647e4
AndrewH2O <AndrewH2O@users.noreply.github.com>
2020-02-03 19:37:49 +0000

    implement spring data service for vet speciality added annotations service and profile.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicv2/services/springdatajpa/SpecialtySDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicv2/services/springdatajpa/SpecialtySDJpaService.java
@@ -3,10 +3,14 @@ package guru.springframework.sfgpetclinicv2.services.springdatajpa;
 import guru.springframework.sfgpetclinicv2.model.Speciality;
 import guru.springframework.sfgpetclinicv2.repositories.SpecialtyRepository;
 import guru.springframework.sfgpetclinicv2.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class SpecialtySDJpaService implements SpecialityService {
     private final SpecialtyRepository specialtyRepository;
 
